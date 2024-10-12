6bb46c2d08567bf46904b3eda96def34bc278ddd
samvardhan <samvardhan777@gmail.com>
2024-10-12 17:50:28 +0530

    Manual Docusaurus deployment

---
+++ assets/js/a06e557d.ec5223b3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdata_dynamos_github_io=self.webpackChunkdata_dynamos_github_io||[]).push([[272],{4061:a=>{a.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://data-dynamos.github.io',
+  url: 'https://data-navigators.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
